0afdddeb4f366d5370208e11e6717bf3ab1bd997
Radostin Handarov <radostin.handarov@gmail.com>
2025-12-22 19:16:47 +0200

    Make app configuration accept db credentials via env variables

--- deployment/app_deployment.tf
+++ deployment/app_deployment.tf
@@ -4,7 +4,7 @@ resource "kubernetes_deployment_v1" "app_deployment" {
     namespace = kubernetes_namespace_v1.myapp.metadata[0].name
   }
   spec {
-    replicas = 2
+    replicas = 1
     selector {
       match_labels = {
         app = "myapp"
@@ -25,6 +25,18 @@ resource "kubernetes_deployment_v1" "app_deployment" {
           name = "myapp"
           image = "myapp:0.0.1"
           image_pull_policy = "Never"
+          env {
+            name = "DB_URL"
+            value = "jdbc:mysql://db-service.myapp-namespace.svc.cluster.local:3306/sports_tournament_organizer?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false"
+          }
+          env {
+            name = "DB_USERNAME"
+            value = "root"
+          }
+          env {
+            name = "DB_PASSWORD"
+            value = "secret"
+          }
           port {
             container_port = 8080
           }
