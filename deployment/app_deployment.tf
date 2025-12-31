58ee6dbd81f4cfebd5323fd1d6327845ca388ce9
Radostin Handarov <radostin.handarov@gmail.com>
2025-12-31 15:08:41 +0200

    Remove comments from app_deployment.tf

--- deployment/app_deployment.tf
+++ deployment/app_deployment.tf
@@ -24,24 +24,11 @@ resource "kubernetes_deployment_v1" "app_deployment" {
         container {
           name = "myapp"
           image = "ghcr.io/rhandarov/myapp:latest"
-          # image_pull_policy = "Never"
-          # env {
-          #   name = "DB_URL"
-          #   value = "jdbc:mysql://db-service.myapp-namespace.svc.cluster.local:3306/sports_tournament_organizer?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true&useLegacyDatetimeCode=false"
-          # }
           env_from {
             config_map_ref {
               name = "db-config-map"
             }
           }
-          # env {
-          #   name = "DB_USERNAME"
-          #   value = "root"
-          # }
-          # env {
-          #   name = "DB_PASSWORD"
-          #   value = "secret"
-          # }
           env_from {
             secret_ref {
               name = "db-secrets"
